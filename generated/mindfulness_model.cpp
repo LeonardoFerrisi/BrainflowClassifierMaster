402a509187912561792323ced852d92d988ffe6d
Ha Truong <125408638+HaTruong05@users.noreply.github.com>
2023-04-22 16:52:15 -0400

    updated and succesfully created 6 ML models for focus - nonfocus

---
+++ generated/mindfulness_model.cpp
@@ -0,0 +1,6 @@
+
+#include "mindfulness_model.h"
+// clang-format off
+const double mindfulness_coefficients[5] = {-0.21021261893492735,-6.308009328547674,13.323278992424163,-5.8390355751456555,-3.3142770117664035};
+double mindfulness_intercept = -0.093930;
+// clang-format on
